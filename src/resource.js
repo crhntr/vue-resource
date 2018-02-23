2865bd15ffd04b21b88110ce8ad49a5e964cceff
crhntr <hunter.christopher@icloud.com>
2018-02-23 01:48:01 -0800

    Edited the Standard Actions for Resources
    I like this better.

--- src/resource.js
+++ src/resource.js
@@ -66,11 +66,9 @@ function opts(action, args) {
 
 Resource.actions = {
 
-    get: {method: 'GET'},
-    save: {method: 'POST'},
-    query: {method: 'GET'},
+    create: {method: 'POST'},
+    read: {method: 'GET'}
     update: {method: 'PATCH'},
-    remove: {method: 'DELETE'},
     delete: {method: 'DELETE'}
 
 };
